99e200b6bcc9b02f6346cfb97a5833aa26734af2
Marco Lewis <marco-lewis@hotmail.com>
2023-10-26 13:53:09 +0100

    Seperate Grover versions

--- faulty_grover.py
+++ faulty_grover.py
@@ -28,9 +28,8 @@ diffusion_oracle = 2*temp - diffusion_oracle
 
 hadamard = np.dot(np.array([[1,1],[1,-1]]), 1/np.sqrt(2))
 hadamard_n = lambda n: hadamard if n == 1 else np.kron(hadamard, hadamard_n(n-1))
-diffusion = np.dot(hadamard_n(n), np.dot(diffusion_oracle, hadamard_n(n)))
-grover = np.dot(diffusion, oracle)
-circuit = [oracle, diffusion]
+faulty_grover = np.dot(np.dot(np.kron(hadamard_n(n-1),np.eye(2,2)), np.dot(diffusion_oracle, hadamard_n(n))), oracle)
+circuit = [oracle, np.kron(hadamard_n(n-1),np.eye(2,2)), diffusion_oracle, hadamard_n(n)]
 
 Z = [sym.Symbol('z' + str(i), complex=True) for i in range(N)]
 variables = Z + [z.conjugate() for z in Z]
@@ -38,16 +37,14 @@ variables = Z + [z.conjugate() for z in Z]
 sum_probs = np.sum([Z[j] * sym.conjugate(Z[j]) for j in range(N)])
 
 g_u = [
-    Z[1] * sym.conjugate(Z[1]) - 0.9,
-    -np.prod([0.9 - Z[i] * sym.conjugate(Z[i]) if not(i == mark) else 1 for i in range(N)]),
+    Z[mark] * sym.conjugate(Z[mark]) - 0.9,
     1 - sum_probs,
     sum_probs - 1,
 ]
 g_u = to_poly(g_u, variables)
 
 d = 0.01
-g_init = []
-g_init += [z * sym.conjugate(z) - (1/N - d) for z in Z]
+g_init = [z * sym.conjugate(z) - (1/N - d) for z in Z]
 g_init += [1/N + d - z * sym.conjugate(z) for z in Z]
 g_init += [-1j * (z - sym.conjugate(z)) for z in Z]
 g_init += [1j * (z - sym.conjugate(z)) for z in Z]
