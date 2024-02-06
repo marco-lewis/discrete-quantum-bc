2453d2143b55c61ada583bf667dccfff2a4862b6
Marco Lewis <marco-lewis@hotmail.com>
2024-02-06 12:38:17 +0000

    Rename b to constraint_val and change commented code

--- src/check.py
+++ src/check.py
@@ -38,9 +38,9 @@ def check_barrier(unitary_barrier_pairs : list[tuple[np.ndarray, sym.Poly]],
     z3_k_diffs = [_sympy_poly_to_z3(var_z3_dict, sym.poly(unitary_barrier_pairs[i2][1].subs(zip(Z, np.dot(unitary_k, Z))) - unitary_barrier_pairs[i1][1], variables, domain=sym.CC)) for unitary_k, i1, i2 in chunks]
 
     z3_constraints : dict[str, z3.ExprRef] = {}
-    # for key in g: z3_constraints[key] = [_sympy_poly_to_z3(var_z3_dict, p).r >= 0 for p in g[key]]
+    for key in g: z3_constraints[key] = [_sympy_poly_to_z3(var_z3_dict, p).r >= 0 for p in g[key]]
     # Alternative to find values of g when solving
-    for key in g: z3_constraints[key] = [z3.And(Complex('g' + key + str(idx)) == _sympy_poly_to_z3(var_z3_dict, poly), Complex('g' + key + str(idx)).r >= 0) for idx, poly in enumerate(g[key])]
+    # for key in g: z3_constraints[key] = [z3.And(Complex('g' + key + str(idx)) == _sympy_poly_to_z3(var_z3_dict, poly), Complex('g' + key + str(idx)).r >= 0) for idx, poly in enumerate(g[key])]
 
     def _check(s : z3.Solver, conds : list[z3.ExprRef], tool=Z3, delta=0.001):
         s.push()
@@ -53,7 +53,7 @@ def check_barrier(unitary_barrier_pairs : list[tuple[np.ndarray, sym.Poly]],
             model = str(s.model())
         elif tool == DREAL:
             logger.info("Using dreal")
-            sat, model = run_dreal(s, delta=delta, log_level=log_level, timeout=600)
+            sat, model = run_dreal(s, delta=delta, log_level=log_level, timeout=300)
         else: logger.error("No tool selected")
         if sat in [z3.unsat, DREAL_UNSAT]: logger.info("Constraint satisfied.")
         elif sat in [z3.sat, DREAL_SAT]: raise_error("Counter example: " + model)
@@ -62,26 +62,26 @@ def check_barrier(unitary_barrier_pairs : list[tuple[np.ndarray, sym.Poly]],
     
     tactic = z3.Then('solve-eqs','smt')
     s = tactic.solver()
-    b = Complex('b')
+    constraint_val = Complex('constraint_val')
     for unitary, z3_barrier in z3_barriers:
         logger.info("Checking barrier for unitary\n" + str(unitary))
         logger.info("Check barrier real")
-        _check(s, [z3.And(z3_constraints[INVARIANT]), b == z3_barrier, z3.Not(b.i == 0)], tool=DREAL)
+        _check(s, [z3.And(z3_constraints[INVARIANT]), constraint_val == z3_barrier, z3.Not(constraint_val.i == 0)], tool=DREAL)
         logger.info("Check " + INIT)
-        _check(s, [z3.And(z3_constraints[INIT]), b == z3_barrier, b.r > 0], tool=DREAL)
+        _check(s, [z3.And(z3_constraints[INIT]), constraint_val == z3_barrier, constraint_val.r > 0], tool=DREAL)
         logger.info("Check " + UNSAFE)
-        _check(s, [z3.And(z3_constraints[UNSAFE]), b == z3_barrier, b.r < d], tool=DREAL)
+        _check(s, [z3.And(z3_constraints[UNSAFE]), constraint_val == z3_barrier, constraint_val.r < d], tool=DREAL)
     # TODO: Change delta (delta = 1e-20) or run z3 (hanging?)
     for idx, z3_diff in enumerate(z3_diffs):
         logger.info("Check " + DIFF + str(idx))
-        _check(s, [z3.And(z3_constraints[INVARIANT]), b == z3_diff, b.r > eps], tool=DREAL)
+        _check(s, [z3.And(z3_constraints[INVARIANT]), constraint_val == z3_diff, constraint_val.r > eps], tool=DREAL)
     for idx, z3_change in enumerate(z3_changes):
         logger.info("Check " + CHANGE + str(idx))
-        _check(s, [z3.And(z3_constraints[INVARIANT]), b == z3_change, b.r > gamma], tool=DREAL)
+        _check(s, [z3.And(z3_constraints[INVARIANT]), constraint_val == z3_change, constraint_val.r > gamma], tool=DREAL)
     # TODO: Change delta (delta = 1e-20) or run z3 (hanging?)
     for idx, z3_k_diff in enumerate(z3_k_diffs):
         logger.info("Check " + INDUCTIVE + str(idx))
-        _check(s, [z3.And(z3_constraints[INVARIANT]), b == z3_k_diff, b.r > 0], tool=DREAL)
+        _check(s, [z3.And(z3_constraints[INVARIANT]), constraint_val == z3_k_diff, constraint_val.r > 0], tool=DREAL)
     logger.info("All constraints checked.")
 
 # Based on: https://stackoverflow.com/a/38980538/19768075
