205b33f7b17b2094266131f3692fa04c3286a8df
Marco Lewis <marco-lewis@hotmail.com>
2024-02-27 14:50:06 +0000

    Added SX example

--- examples/gates.py
+++ examples/gates.py
@@ -3,6 +3,7 @@ import numpy as np
 xgate = np.array([[0,1],[1,0]])
 zgate = np.array([[1,0],[0,-1]])
 tgate = np.array([[1,0],[0,np.exp(1j*np.pi/4)]])
+sxgate = 1/2 * np.array([[1+1j, 1-1j], [1-1j, 1+1j]])
 
 hadamard = np.dot(np.array([[1,1],[1,-1]]), 1/np.sqrt(2))
 hadamard_n = lambda n: hadamard if n == 1 else np.kron(hadamard, hadamard_n(n-1))
