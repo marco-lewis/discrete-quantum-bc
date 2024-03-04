0bdc860ffad166b600e0b7ab6fca78e2128cce3f
Marco Lewis <marco-lewis@hotmail.com>
2024-03-04 13:54:14 +0000

    Change shell script

--- examples/examples.sh
+++ examples/examples.sh
@@ -11,5 +11,7 @@ python -m examples.run_example -ex xzgate --check -n 1 -k 2 --target 0
 python -m examples.run_example -ex xzgate --check -n 1 -k 2 --target 1
 python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 0
 # Grover examples
-python -m examples.run_example -ex groverop_unmark_low --check -n 2 -k 1 --target 3 --mark 2
-python -m examples.run_example -ex groverop_unmark_low --check -n 2 -k 1 --target 2 --mark 1
+python -m examples.run_example -ex grover_odd_unmark --check -n 2 -k 1 --target 3 --mark 2
+python -m examples.run_example -ex grover_even_unmark --check -n 2 -k 1 --target 3 --mark 2
+python -m examples.run_example -ex grover_odd_unmark --check -n 2 -k 1 --target 0 --mark 1
+python -m examples.run_example -ex grover_even_unmark --check -n 2 -k 1 --target 0 --mark 1
