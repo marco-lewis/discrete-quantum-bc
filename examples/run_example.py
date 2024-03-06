fcef7830379f13807783006201971832cc340522
Marco Lewis <marco-lewis@hotmail.com>
2024-03-06 11:17:28 +0000

    Change call so not all one line

--- examples/run_example.py
+++ examples/run_example.py
@@ -39,7 +39,11 @@ def run_example(file_tag : str,
         logger.info("g defined")
         logger.debug(g)
         
-        barrier_certificate, times = find_barrier_certificate(circuit, g, Z, barrier_degree=barrier_degree, eps=epsilon, gamma=gamma, k=k, verbose=verbose, log_level=log_level, precision_bound=precision_bound, solver=solver, check=check)
+        barrier_certificate, times = find_barrier_certificate(
+            circuit, g, Z, barrier_degree=barrier_degree, eps=epsilon,
+            gamma=gamma, k=k, verbose=verbose, log_level=log_level,
+            precision_bound=precision_bound, solver=solver, check=check
+            )
         logger.info("Barrier certificate: " +  str(barrier_certificate))
         with open("logs/barrier_" + file_tag + ".log", 'w') as file:
             file.write("\n".join([
