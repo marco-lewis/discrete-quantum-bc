a5b82df126a4dd78e6db9eb19ed45b33e19352f7
Marco Lewis <marco-lewis@hotmail.com>
2024-04-02 11:43:42 +0100

    Update experiments

--- examples/examples.py
+++ examples/examples.py
@@ -16,9 +16,8 @@ def Z_example(Z : list[sym.Symbol], variables : list[sym.Symbol], n=1, k=1, targ
         1j*(Z[0] - sym.conjugate(Z[0])),
         -1j*(Z[0] - sym.conjugate(Z[0])),
         ]
-    g_u = [
-        Z[1 - target] * sym.conjugate(Z[1 - target]) - 0.2,
-        ]
+    Z_no_target = Z[:target] + Z[target+1:]
+    g_u = [z * sym.conjugate(z) - 0.2 for z in Z_no_target]
     g_init = poly_list(g_init, variables)
     g_u = poly_list(g_u, variables)
 
@@ -49,16 +48,14 @@ def X_example(Z : list[sym.Symbol], variables : list[sym.Symbol], n=1, k=1, targ
     g[INIT] = g_init
     return file_tag, circuit, g
 
-# experiment = 0, 1 good for n=1
-# n=2, experiment = 0 good, = 1, 3 bad
+# target = 0, 1 good for n=1
+# n=2, experiment = 0 good; = 1, 2, 3 bad
 def XZ_example(Z : list[sym.Symbol], variables : list[sym.Symbol], n=1, k=1, target=0):
     file_tag = f"xz{n}_k{k}_tgt{target}"
 
     circuit = [n_gate(Xgate, n), n_gate(Zgate, n)] * 6
 
-    g_init = [
-        Z[target] * sym.conjugate(Z[target]) - 0.9,
-        ]
+    g_init = [Z[target] * sym.conjugate(Z[target]) - 0.9]
     g_u = [
         0.8 - Z[target] * sym.conjugate(Z[target]),
         Z[target] * sym.conjugate(Z[target]) - 0.2,
@@ -71,10 +68,12 @@ def XZ_example(Z : list[sym.Symbol], variables : list[sym.Symbol], n=1, k=1, tar
     g[INIT] = g_init
     return file_tag, circuit, g
 
+# Does not work (m, tgt): (2,3)
 def Grover_unmark_example(Z : list[sym.Symbol], variables : list[sym.Symbol], n=1, k=1, target=0, mark=1, odd=0):
     N = 2**n
     odd_str = "odd" if odd else "even"
-    file_tag = f"grover_unmark{n}_{odd_str}_m{mark}_tgt{target}"
+    file_tag = f"grover_unmark{n}_{odd_str}_k{k}_m{mark}_tgt{target}"
+    if target == mark: raise Exception("target value needs to be different from mark value.")
 
     oracle = np.eye(N, N)
     oracle[mark, mark] = -1
@@ -91,10 +90,10 @@ def Grover_unmark_example(Z : list[sym.Symbol], variables : list[sym.Symbol], n=
 
     err = 10 ** -(n+1)
     g_init = []
-    g_init += [(z + sym.conjugate(z))/2 - np.sqrt(1/N - err) for z in Z]
-    g_init += [np.sqrt(1/N + err) - (z + sym.conjugate(z))/2 for z in Z]
-    g_init += [1 - np.sum([((z + sym.conjugate(z))/2)**2 for z in Z])]
-    g_init += [np.sum([((z + sym.conjugate(z))/2)**2 for z in Z]) - 1]
+    g_init += [(z * sym.conjugate(z)) - (1/N - err) for z in Z]
+    g_init += [1/N + err - (z * sym.conjugate(z)) for z in Z]
+    g_init += [(z - sym.conjugate(z))/2j for z in Z]
+    g_init += [np.sqrt(err) - (z - sym.conjugate(z))/2j for z in Z]
     if odd: g_init = [g.subs(zip(Z, np.dot(oracle, Z))) for g in g_init]
     g_init = poly_list(g_init, variables)
 
