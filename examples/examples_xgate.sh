9150efe44014deaba28c3545cd103a91129efbdc
Marco Lewis <marco-lewis@hotmail.com>
2024-04-16 00:07:55 +0100

    X gate script

--- examples/examples_xgate.sh
+++ examples/examples_xgate.sh
@@ -1,3 +1,7 @@
 # X gate examples
-python -m examples.run_example -ex xgate --check -n 1 -k 1 --target 0
-python -m examples.run_example -ex xgate --check -n 2 -k 1 --target 0
+python -m examples.run_example -ex xgate --check -n 1 -k 2 --target 0 --runs 5 --gamma 0 --smt-timeout 300
+python -m examples.run_example -ex xgate --check -n 1 -k 2 --target 1 --runs 5 --gamma 0 --smt-timeout 300
+python -m examples.run_example -ex xgate --check -n 2 -k 2 --target 0 --runs 5 --gamma 0 --smt-timeout 300 --barrier-degree 2
+python -m examples.run_example -ex xgate --check -n 2 -k 2 --target 1 --runs 5 --gamma 0 --smt-timeout 300 --barrier-degree 2
+python -m examples.run_example -ex xgate --check -n 2 -k 2 --target 2 --runs 5 --gamma 0 --smt-timeout 300 --barrier-degree 2
+python -m examples.run_example -ex xgate --check -n 2 -k 2 --target 3 --runs 5 --gamma 0 --smt-timeout 300 --barrier-degree 2
