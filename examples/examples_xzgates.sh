9bce36d9494dbc465fdbdf30f406473a35bac1ec
Marco Lewis <marco-lewis@hotmail.com>
2024-04-17 10:59:43 +0100

    XZ gates example script

--- examples/examples_xzgates.sh
+++ examples/examples_xzgates.sh
@@ -1,7 +1,7 @@
 # XZ gate examples
-python -m examples.run_example -ex xzgate --check -n 1 -k 2 --target 0
-python -m examples.run_example -ex xzgate --check -n 1 -k 2 --target 1
-python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 0
-python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 1
-python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 2
-python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 3
+python -m examples.run_example -ex xzgate --check -n 1 -k 2 --target 0 --runs 5
+python -m examples.run_example -ex xzgate --check -n 1 -k 2 --target 1 --runs 5
+python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 0 --runs 5
+python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 1 --runs 5 --smt-timeout 300 --barrier-degree 4
+python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 2 --runs 5 --smt-timeout 300 --barrier-degree 4
+python -m examples.run_example -ex xzgate --check -n 2 -k 2 --target 3 --runs 5 --smt-timeout 300 --barrier-degree 4
