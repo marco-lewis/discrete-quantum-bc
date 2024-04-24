c3c4e3503857cc2cd0470e18ee9fcd9d9a1ab6a0
Marco Lewis <marco-lewis@hotmail.com>
2024-04-24 10:17:33 +0100

    Update grover examples script

--- examples/examples_grover_unmark.sh
+++ examples/examples_grover_unmark.sh
@@ -1,5 +1,7 @@
 # Grover unmark examples
-python -m examples.run_example -ex grover_odd_unmark --check -n 2 -k 1 --target 3 --mark 2
-python -m examples.run_example -ex grover_even_unmark --check -n 2 -k 1 --target 3 --mark 2
-python -m examples.run_example -ex grover_odd_unmark --check -n 2 -k 1 --target 0 --mark 1
-python -m examples.run_example -ex grover_even_unmark --check -n 2 -k 1 --target 0 --mark 1
+# Fails for longer timeout
+# Tryout using only superposition state (or super w/ positive real+imag phases)
+python -m examples.run_example -ex grover_dual --check -n 2 -k 1 --target 1 --mark 0 --runs 5
+python -m examples.run_example -ex grover_dual --check -n 2 -k 2 --target 1 --mark 0 --runs 5
+python -m examples.run_example -ex grover_odd_unmark --check -n 2 -k 1 --target 1 --mark 0 --runs 5
+python -m examples.run_example -ex grover_even_unmark --check -n 2 -k 1 --target 1 --mark 0 --runs 5
