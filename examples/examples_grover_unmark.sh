d4677d83be9d6646c58950e6a297c5e1336ec666
Marco Lewis <marco-lewis@hotmail.com>
2024-04-24 10:34:33 +0100

    Remove comments

--- examples/examples_grover_unmark.sh
+++ examples/examples_grover_unmark.sh
@@ -1,6 +1,4 @@
 # Grover unmark examples
-# Fails for longer timeout
-# Tryout using only superposition state (or super w/ positive real+imag phases)
 python -m examples.run_example -ex grover_dual --check -n 2 -k 1 --target 1 --mark 0 --runs 5
 python -m examples.run_example -ex grover_dual --check -n 2 -k 2 --target 1 --mark 0 --runs 5
 python -m examples.run_example -ex grover_odd_unmark --check -n 2 -k 1 --target 1 --mark 0 --runs 5
